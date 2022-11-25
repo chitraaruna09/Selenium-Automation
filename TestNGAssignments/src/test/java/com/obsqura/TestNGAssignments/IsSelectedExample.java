7bbb8bf4d9b275d898fe20aa0d37f88369731e1a
chitraaruna09@gmail.com <chitraaruna09@gmail.com>
2022-11-25 08:59:56 +0530

    assignment works added

---
+++ TestNGAssignments/src/test/java/com/obsqura/TestNGAssignments/IsSelectedExample.java
@@ -0,0 +1,43 @@
+package com.obsqura.TestNGAssignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class IsSelectedExample extends DriverLoading{
+
+	@Test
+	public void verifyCheckBoxIsSelected() {
+		WebElement redCheckbox;
+		redCheckbox = driver.findElement(By.xpath("(//input[@name='color'])[1]"));
+		if(redCheckbox.isSelected()==false) {
+			driver.findElement(By.xpath("(//input[@name='color'])[1]")).click();
+			Assert.assertTrue(redCheckbox.isSelected(), "Red Check Box is not Selected");
+		}
+		Assert.assertTrue(redCheckbox.isSelected(), "Red Check Box is Selected");
+	}
+	
+	@Test
+	public void verifyCheckBoxIsEnabled() {
+		WebElement redCheckbox;
+		redCheckbox = driver.findElement(By.xpath("(//input[@name='color'])[1]"));
+		if(redCheckbox.isEnabled()==false) {
+			driver.findElement(By.xpath("(//input[@name='color'])[1]")).isEnabled();
+			Assert.assertTrue(redCheckbox.isEnabled(), "Red Check Box is not Enabled");
+		}
+		Assert.assertTrue(redCheckbox.isEnabled(), "Red Check Box is Enabled");
+	}
+	
+	@Test
+	public void verifyOperaRadioButtonIsSelected() {
+		WebElement operaRadioButton;
+		operaRadioButton = driver.findElement(By.xpath("(//input[@name='browser'])[3]"));
+		if(operaRadioButton.isSelected()==false) {
+			operaRadioButton.click();;
+			Assert.assertTrue(operaRadioButton.isSelected(), "Opera Radio Button is not Selected");
+		}
+		Assert.assertTrue(operaRadioButton.isSelected(), "Some other radio button is selected");
+	}
+	
+}
